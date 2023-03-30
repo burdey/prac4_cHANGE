e8fdd8b42d595e5ee958e740c3172472fe13f9ff
burdey <88636823+burdey@users.noreply.github.com>
2023-03-31 02:16:40 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,44 @@ router.get('/last.txt', function(req, res, next) {
   res.send(date);
 });
 
+var color_number = 0;
+router.get('/color.html', function(req,res,next){
+  let page ='\
+  <!DOCTYPE html>\
+    <html>\
+      <head>\
+        <title>task3-2</title>\
+      </head>\
+      <body>\
+        <h1 id="color"></h1>\
+        <script>\
+        var C = document.getElementById("color");\
+        var color_number='+ color_number +';\
+        if(color_number == 0){\
+          C.innerText = "red";\
+          C.style.color = "red";\
+        }\
+        else if(color_number == 1){\
+          C.innerText = "yellow";\
+          C.style.color = "yellow";\
+        }\
+        else if(color_number == 2){\
+          C.innerText = "green";\
+          C.style.color = "green";\
+        }\
+        else if(color_number == 3){\
+          C.innerText = "blue";\
+          C.style.color = "blue";\
+        }\
+        </script>\
+      </body>\
+  </html>\
+'
+color_number++;
+if(color_number == 4){
+  color_number = 0;
+}
+res.send(page);
+});
+
 module.exports = router;
