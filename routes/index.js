50810a86fba0b51bb17e74d578fc1d2c5264968c
burdey <88636823+burdey@users.noreply.github.com>
2023-03-31 02:12:51 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/last.txt', function(req, res, next) {
-  var date = new Date().toString();
-  res.send(date);
-  var date=new Date().toString();
-});
-
 module.exports = router;
